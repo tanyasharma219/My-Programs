fc0ae58958dd7fa841b30da70808c42c782e157e
tanyasharma219 <63143037+tanyasharma219@users.noreply.github.com>
2020-04-05 14:42:37 -0400

    Create hello_world.cc

---
+++ C++Programs/hello_world.cc
@@ -0,0 +1,11 @@
+ques.simple program to practice printing to stdout. 
+
+ans:
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
