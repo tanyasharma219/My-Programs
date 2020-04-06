acd07d98599c59ab09ff526e0f235281c9927d27
tanyasharma219 <63143037+tanyasharma219@users.noreply.github.com>
2020-04-06 05:26:43 -0400

    Create sed_command_1.sh

---
+++ Linux_Shell/sed_command_1.sh
@@ -0,0 +1,8 @@
+SED:  Sed is a popular tool that enables quick parsing and transformation of text.
+TASK-  we will practice using the sed command to parse and transform text.
+
+ans:
+# Last modified : 18/02/2020
+# Author : Nicolas Richard (Nicolas.richard.dev@gmail.com)
+
+sed -e 's/\<the\>/this/1'
