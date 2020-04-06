814a2327ef50fbc27ec5fc46986dfa7b7849aeb9
tanyasharma219 <63143037+tanyasharma219@users.noreply.github.com>
2020-04-06 09:12:52 -0400

    Create count.sh
    Count the number of elements in an Array

---
+++ Linux_Shell/count.sh
@@ -0,0 +1,7 @@
+task-   Given a list of countries, each on a new line, your task is to read them into an array and then display the count of elements 
+        in that array.
+        
+ans-
+#Given a list of countries, each on a new line, your task is to read them into an array and then display the count of elements in that array.
+arr=($(cat))
+echo ${#arr[@]}
