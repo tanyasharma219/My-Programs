40d071cc64109724fbdfcad5a0ac466e5475b6e2
tanyasharma219 <63143037+tanyasharma219@users.noreply.github.com>
2020-04-06 05:28:44 -0400

    Create sed_command_2.sh

---
+++ Linux_Shell/sed_command_2.sh
@@ -0,0 +1,17 @@
+explaintion: 
+The line:
+
+"Feed'st thy light's flame with self-substantial fuel," 
+has been transformed to:
+
+"Feed'st your light's flame with self-substantial fuel,"  
+The line:
+
+"Thy self thy foe, to thy sweet self too cruel:" 
+has been transformed to:
+
+"your self your foe, to your sweet self too cruel:"
+
+
+ans:
+sed 's/thy/your/ig'
