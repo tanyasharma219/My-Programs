159cffbac513b70d4e72f730eff5fe8ed05a6d58
tanyasharma219 <63143037+tanyasharma219@users.noreply.github.com>
2020-04-06 09:11:14 -0400

    Create display.sh

---
+++ Linux_Shell/display.sh
@@ -0,0 +1,9 @@
+task-  Given a list of countries, each on a new line, your task is to read them into an array and then display the element indexed at 3.
+       Note that indexing starts from 0.
+       
+       
+       
+ans-
+# Given a list of countries, each on a new line, your task is to read them into an array and then display the element indexed at . Note that indexing starts from 0.
+arr=($(cat))
+echo ${arr[3]}
